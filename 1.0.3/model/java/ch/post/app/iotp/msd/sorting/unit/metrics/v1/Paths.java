affa850a348ed76ede3fb4fdaa3fe25f431cb7f9
bierier <erich.bieri@post.ch>
2025-07-22 17:39:20 +0200

    Removed java and avro files

--- 1.0.3/model/java/ch/post/app/iotp/msd/sorting/unit/metrics/v1/Paths.java
+++
@@ -1,8 +0,0 @@
-package ch.post.app.iotp.msd.sorting.unit.metrics.v1;
-
-/**
- * @deprecated Use MSDSortingUnitMetricsStreamAPIPaths, MSDSortingUnitMetricsStreamAPIPathVariables or MSDSortingUnitMetricsStreamAPIPathBuilder instead.
- */
-@Deprecated
-public final class Paths {
-}
